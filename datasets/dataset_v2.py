fb82099a73c34ed407c210f6c3597fae9b5b9441
Stefan Klut <stefanklut12@gmail.com>
2022-09-22 18:06:52 +0200

    Dataset wirh multithread and txt files

--- datasets/dataset_v2.py
+++ datasets/dataset_v2.py
@@ -1,5 +1,78 @@
 import os
 
 import numpy as np
+import argparse
+import ast
 
-def dataset_dict_loader():
+from pathlib import Path
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description="Preprocessing an annotated dataset of documents with pageXML")
+    parser.add_argument("-i", "--input", help="Input folder",
+                        required=True, type=str)
+
+    args = parser.parse_args()
+    return args
+
+def create_data(paths):
+    pass
+
+def dataset_dict_loader(dataset_dir: str | Path):
+    if isinstance(dataset_dir, str):
+        dataset_dir = Path(dataset_dir)
+
+    image_list = dataset_dir.joinpath("image_list.txt")
+    if not image_list.is_file():
+        raise FileNotFoundError(f"Image list is missing ({image_list})")
+
+    mask_list = dataset_dir.joinpath("mask_list.txt")
+    if not mask_list.is_file():
+        raise FileNotFoundError(f"Mask list is missing ({mask_list})")
+
+    output_sizes_list = dataset_dir.joinpath("output_sizes.txt")
+    if not output_sizes_list.is_file():
+        raise FileNotFoundError(
+            f"Output sizes is missing ({output_sizes_list})")
+
+    with open(image_list, mode='r') as f:
+        image_paths = [Path(line) for line in f.readlines()]
+
+    with open(mask_list, mode='r') as f:
+        mask_paths = [Path(line) for line in f.readlines()]
+
+    with open(output_sizes_list, mode='r') as f:
+        output_sizes = f.readlines()
+        output_sizes = [ast.literal_eval(output_size)
+                        for output_size in output_sizes]
+        print(output_sizes)
+
+    # Data formatting check
+    if not (len(image_paths) == len(mask_paths) == len(output_sizes)):
+        raise ValueError(
+            "expecting the images, mask and output_sizes to be the same lenght")
+    
+    print(zip(image_paths, mask_paths))
+
+    for image_path, mask_path in zip(image_paths, mask_paths):
+        # Data existence check
+        if not image_path.is_file():
+            raise FileNotFoundError(f"Image path missing ({image_path})")
+        if not mask_path.is_file():
+            raise FileNotFoundError(f"Mask path missing ({mask_path})")
+
+        # Data_ids check
+        if image_path.stem != mask_path.stem:
+            raise ValueError(
+                f"Image id should match mask id ({image_path.stem} vs {mask_path.stem}")
+
+
+def main(args):
+    results = dataset_dict_loader(args.input)
+    print(results)
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    main(args)
--- datasets/preprocess_v2.py
+++ datasets/preprocess_v2.py
@@ -1,3 +1,4 @@
+
 from tqdm import tqdm
 import argparse
 import glob
@@ -6,12 +7,13 @@ import pathlib
 import pickle
 import sys
 from pathlib import Path
-from typing import List, Optional
+from typing import Optional
 import cv2
 
 import matplotlib
 import numpy as np
 from natsort import os_sorted
+from multiprocessing import Pool
 
 sys.path.append(str(Path(__file__).resolve().parent.joinpath("..")))
 from page_xml.xmlPAGE import PageData
@@ -20,19 +22,30 @@ from page_xml.xmlPAGE import PageData
 def get_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Preprocessing an annotated dataset of documents with pageXML")
-    parser.add_argument("-i", "--input", help="Input folder", required=True)
-    parser.add_argument("-o", "--output", help="Output folder", required=True)
-    parser.add_argument("-m", "--mode", help="Output mode", choices=["baseline", "region", "both"], default="baseline")
-    parser.add_argument("-r", "--resize", action="store_true", help="Resize input images")
+    parser.add_argument("-i", "--input", help="Input folder",
+                        required=True, type=str)
+    parser.add_argument(
+        "-o", "--output", help="Output folder", required=True, type=str)
+    parser.add_argument("-m", "--mode", help="Output mode",
+                        choices=["baseline", "region", "both"], default="baseline", type=str)
+    parser.add_argument("-r", "--resize", action="store_true",
+                        help="Resize input images")
+    parser.add_argument("-w", "--line_width",
+                        help="Used line width", type=int, default=5)
+    parser.add_argument("-c", "--line_color", help="Used line color",
+                        choices=list(range(256)), type=int, metavar="{0-255}", default=1)
 
     args = parser.parse_args()
     return args
 
 
 class Preprocess:
-    def __init__(self, input_dir=None, output_dir=None, mode="baseline", auto_resize=False) -> None:
+    def __init__(self, input_dir=None, output_dir=None, mode="baseline", auto_resize=False, line_width=5, line_color=1) -> None:
         self.input_dir: Optional[Path] = None
         self.output_dir: Optional[Path] = None
+        self.line_width = line_width
+        self.line_color = line_color
+        self.total_size = 2048*2048
         self.mode = mode
         # Formats found here: https://docs.opencv.org/4.x/d4/da8/group__imgcodecs.html#imread
         self.image_formats = [".bmp", ".dib",
@@ -96,7 +109,7 @@ class Preprocess:
         return self.output_dir
 
     @staticmethod
-    def check_pageXML_exists(image_paths: List[Path]) -> None:
+    def check_pageXML_exists(image_paths: list[Path]) -> None:
         xml_paths = [image_path.parent.joinpath("page", image_path.stem).with_suffix(
             '.xml') for image_path in image_paths]
 
@@ -119,12 +132,12 @@ class Preprocess:
             width = np.ceil(origwidth / (256 * counter)) * 256
             counter += 1
 
-        res_image = cv2.resize(image, (width, height),
+        res_image = cv2.resize(image, np.asarray([width, height]).astype(np.int32),
                                interpolation=cv2.INTER_CUBIC)
 
         return res_image
 
-    def process_single_file(self, image_path: Path) -> None:
+    def process_single_file(self, image_path: Path) -> tuple[str, str, np.ndarray]:
         if self.input_dir is None:
             raise ValueError("Cannot run when the input dir is not set")
         if self.output_dir is None:
@@ -137,26 +150,34 @@ class Preprocess:
         image = cv2.imread(str(image_path))
 
         if self.auto_resize:
-            image = self.resize_image(image)
-            
+            image = self.resize_image(image, self.total_size)
+
         image_shape = np.asarray(image.shape[:2])
 
-        cv2.imwrite(str(self.output_dir.joinpath(image_stem)) + ".png", image)
-        
+        out_image_path = str(self.output_dir.joinpath(
+            "original", image_stem)) + ".png"
+
+        cv2.imwrite(out_image_path, image)
+
         gt_data = PageData(xml_path)
         gt_data.parse()
-        
+
         if self.mode == "baseline":
-            baseline_mask = gt_data.build_baseline_mask(image_shape, color=1, line_width=3)
+            baseline_mask = gt_data.build_baseline_mask(
+                image_shape, color=self.line_color, line_width=self.line_width)
             mask = baseline_mask
         elif self.mode == "region":
             region_mask = gt_data.build_mask(image_shape, node_types, classes)
             mask = region_mask
         else:
             raise NotImplementedError
-        
-        cv2.imwrite(str(self.output_dir.joinpath(image_stem)) + "_gt.png", mask)
-        
+
+        out_mask_path = str(self.output_dir.joinpath(
+            "ground_truth", image_stem)) + ".png"
+
+        cv2.imwrite(out_mask_path, mask)
+
+        return out_image_path, out_mask_path, image_shape
 
     def run(self) -> None:
         if self.input_dir is None:
@@ -173,12 +194,41 @@ class Preprocess:
 
         self.check_pageXML_exists(image_paths)
 
-        for image_path in tqdm(image_paths):
-            self.process_single_file(image_path)
+        self.output_dir.joinpath("original").mkdir(parents=True, exist_ok=True)
+        self.output_dir.joinpath("ground_truth").mkdir(
+            parents=True, exist_ok=True)
+
+        # Single thread
+        # for image_path in tqdm(image_paths):
+        #     self.process_single_file(image_path)
+
+        # Multithread
+        with Pool(os.cpu_count()) as pool:
+            results = list(tqdm(pool.imap_unordered(
+                self.process_single_file, image_paths), total=len(image_paths)))
+
+        image_list, mask_list, output_sizes = zip(*results)
+
+        with open(self.output_dir.joinpath("image_list.txt"), mode='w') as f:
+            for new_image_path in image_list:
+                f.write(f"{new_image_path}\n")
+
+        with open(self.output_dir.joinpath("mask_list.txt"), mode='w') as f:
+            for new_mask_path in mask_list:
+                f.write(f"{new_mask_path}\n")
+
+        with open(self.output_dir.joinpath("output_sizes.txt"), mode='w') as f:
+            for output_size in output_sizes:
+                f.write(f"{output_size[0]}, {output_size[1]}\n")
 
 
 def main(args):
-    process = Preprocess(args.input, args.output, args.mode, args.resize)
+    process = Preprocess(input_dir=args.input,
+                         output_dir=args.output,
+                         mode=args.mode,
+                         auto_resize=args.resize,
+                         line_width=args.line_width,
+                         line_color=args.line_color)
     process.run()
 
 
