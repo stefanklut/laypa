f12d4039c606f1fba5e4ec850f65b30de257bdf2
Stefan Klut <35296275+stefanklut@users.noreply.github.com>
2022-11-08 17:47:01 +0100

    Update buildImage.sh

--- buildImage.sh
+++ buildImage.sh
@@ -2,19 +2,19 @@
 
 #!/bin/bash
 
-if [ -z $1 ]; then echo "first parameter should be the path of the layout-analysis" && exit 1; fi;
-LAYOUT="$(realpath $1)"
+if [ -z $1 ]; then echo "first parameter should be the path of laypa" && exit 1; fi;
+LAYPA="$(realpath $1)"
 
-docker rmi docker.layout-analysis
+docker rmi docker.laypa
 
 echo "Change to directory of script..."
 DIR_OF_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR_OF_SCRIPT
 
-cp -r $LAYOUT layout
+cp -r $LAYPA laypa
 
 echo "Building docker image..."
-docker build --squash --no-cache . -t docker.layout-analysis
+docker build --squash --no-cache . -t docker.laypa
 
 rm -rf layout
 docker system prune -f
