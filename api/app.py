216c169591f4de958f10e3829c0d22fbffec033a
Stefan Klut <stefanklut12@gmail.com>
2023-02-15 12:41:38 +0100

    move to folder for api

--- api/app.py
+++ api/app.py
@@ -1,27 +1,31 @@
-import torch
-
-import numpy as np
-import cv2
-
-from flask import Flask, jsonify, request
-from page_xml.generate_pageXML import GenPageXML
-from run import Predictor
-from main import setup_cfg
 from dataclasses import dataclass, field
 
+import cv2
+import numpy as np
+import torch
+from flask import Flask, jsonify, request
+
+
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.joinpath("..")))
+from page_xml.generate_pageXML import GenPageXML
+from main import setup_cfg
+from run import Predictor
 
 app = Flask(__name__)
 
 @dataclass
 class DummyArgs():
     config: str = "configs/segmentation/baseline/baseline_dataset_imagenet.yaml"
-    output: str = "test/"
+    output: str = "./api/output/"
     opts: list[str] = field(default_factory=list)
 
 args = DummyArgs()
 
 cfg = setup_cfg(args, save_config=False)
 
+#FIXME generates an empty output folder each time this is called
 gen_page = GenPageXML(output_dir=args.output,
                       mode=cfg.MODEL.MODE,
                       line_width=cfg.PREPROCESS.BASELINE.LINE_WIDTH,
---
+++ api/start_flask.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $( builtin cd "$( dirname ${BASH_SOURCE[0]} )/.."; pwd ) != $( pwd ) ]]; then
+    DIR_OF_SCRIPT=$( builtin cd "$( dirname ${BASH_SOURCE[0]} )/.."; pwd )
+    echo "Change to laypa base folder ($DIR_OF_SCRIPT)"
+    cd $DIR_OF_SCRIPT
+fi
+
+FLASK_DEBUG=true FLASK_APP=api.app.py flask run
