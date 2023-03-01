2734c8083ad99b8a14ee8fd6f572abe04fc9ee2b
Stefan Klut <stefanklut12@gmail.com>
2023-03-01 11:06:47 +0100

    extra defaults mask2former

--- configs/extra_defaults.py
+++ configs/extra_defaults.py
@@ -108,3 +108,107 @@ _C.INPUT.AFFINE.SCALE.PROBABILITY = 0.5
 _C.INPUT.AFFINE.SCALE.STANDARD_DEVIATION = 0.12
 
 
+# Added for mask2former
+_C.INPUT.DATASET_MAPPER_NAME = "mask_former_semantic"
+# Color augmentation
+_C.INPUT.COLOR_AUG_SSD = False
+# We retry random cropping until no single category in semantic segmentation GT occupies more
+# than `SINGLE_CATEGORY_MAX_AREA` part of the crop.
+_C.INPUT.CROP.SINGLE_CATEGORY_MAX_AREA = 1.0
+# Pad image and segmentation GT in dataset mapper.
+_C.INPUT.SIZE_DIVISIBILITY = -1
+
+# solver config
+# weight decay on embedding
+_C.SOLVER.WEIGHT_DECAY_EMBED = 0.0
+# optimizer
+_C.SOLVER.OPTIMIZER = "ADAMW"
+_C.SOLVER.BACKBONE_MULTIPLIER = 0.1
+
+# mask_former model config
+_C.MODEL.MASK_FORMER = CN()
+
+# loss
+_C.MODEL.MASK_FORMER.DEEP_SUPERVISION = True
+_C.MODEL.MASK_FORMER.NO_OBJECT_WEIGHT = 0.1
+_C.MODEL.MASK_FORMER.CLASS_WEIGHT = 1.0
+_C.MODEL.MASK_FORMER.DICE_WEIGHT = 1.0
+_C.MODEL.MASK_FORMER.MASK_WEIGHT = 20.0
+
+# transformer config
+_C.MODEL.MASK_FORMER.NHEADS = 8
+_C.MODEL.MASK_FORMER.DROPOUT = 0.1
+_C.MODEL.MASK_FORMER.DIM_FEEDFORWARD = 2048
+_C.MODEL.MASK_FORMER.ENC_LAYERS = 0
+_C.MODEL.MASK_FORMER.DEC_LAYERS = 6
+_C.MODEL.MASK_FORMER.PRE_NORM = False
+
+_C.MODEL.MASK_FORMER.HIDDEN_DIM = 256
+_C.MODEL.MASK_FORMER.NUM_OBJECT_QUERIES = 100
+
+_C.MODEL.MASK_FORMER.TRANSFORMER_IN_FEATURE = "res5"
+_C.MODEL.MASK_FORMER.ENFORCE_INPUT_PROJ = False
+
+# mask_former inference config
+_C.MODEL.MASK_FORMER.TEST = CN()
+_C.MODEL.MASK_FORMER.TEST.SEMANTIC_ON = True
+_C.MODEL.MASK_FORMER.TEST.INSTANCE_ON = False
+_C.MODEL.MASK_FORMER.TEST.PANOPTIC_ON = False
+_C.MODEL.MASK_FORMER.TEST.OBJECT_MASK_THRESHOLD = 0.0
+_C.MODEL.MASK_FORMER.TEST.OVERLAP_THRESHOLD = 0.0
+_C.MODEL.MASK_FORMER.TEST.SEM_SEG_POSTPROCESSING_BEFORE_INFERENCE = False
+
+# Sometimes `backbone.size_divisibility` is set to 0 for some backbone (e.g. ResNet)
+# you can use this config to override
+_C.MODEL.MASK_FORMER.SIZE_DIVISIBILITY = 32
+
+# pixel decoder config
+_C.MODEL.SEM_SEG_HEAD.MASK_DIM = 256
+# adding transformer in pixel decoder
+_C.MODEL.SEM_SEG_HEAD.TRANSFORMER_ENC_LAYERS = 0
+# pixel decoder
+_C.MODEL.SEM_SEG_HEAD.PIXEL_DECODER_NAME = "BasePixelDecoder"
+
+# swin transformer backbone
+_C.MODEL.SWIN = CN()
+_C.MODEL.SWIN.PRETRAIN_IMG_SIZE = 224
+_C.MODEL.SWIN.PATCH_SIZE = 4
+_C.MODEL.SWIN.EMBED_DIM = 96
+_C.MODEL.SWIN.DEPTHS = [2, 2, 6, 2]
+_C.MODEL.SWIN.NUM_HEADS = [3, 6, 12, 24]
+_C.MODEL.SWIN.WINDOW_SIZE = 7
+_C.MODEL.SWIN.MLP_RATIO = 4.0
+_C.MODEL.SWIN.QKV_BIAS = True
+_C.MODEL.SWIN.QK_SCALE = None
+_C.MODEL.SWIN.DROP_RATE = 0.0
+_C.MODEL.SWIN.ATTN_DROP_RATE = 0.0
+_C.MODEL.SWIN.DROP_PATH_RATE = 0.3
+_C.MODEL.SWIN.APE = False
+_C.MODEL.SWIN.PATCH_NORM = True
+_C.MODEL.SWIN.OUT_FEATURES = ["res2", "res3", "res4", "res5"]
+_C.MODEL.SWIN.USE_CHECKPOINT = False
+
+# NOTE: maskformer2 extra configs
+# transformer module
+_C.MODEL.MASK_FORMER.TRANSFORMER_DECODER_NAME = "MultiScaleMaskedTransformerDecoder"
+
+# LSJ aug
+_C.INPUT.IMAGE_SIZE = 1024
+_C.INPUT.MIN_SCALE = 0.1
+_C.INPUT.MAX_SCALE = 2.0
+
+# MSDeformAttn encoder configs
+_C.MODEL.SEM_SEG_HEAD.DEFORMABLE_TRANSFORMER_ENCODER_IN_FEATURES = ["res3", "res4", "res5"]
+_C.MODEL.SEM_SEG_HEAD.DEFORMABLE_TRANSFORMER_ENCODER_N_POINTS = 4
+_C.MODEL.SEM_SEG_HEAD.DEFORMABLE_TRANSFORMER_ENCODER_N_HEADS = 8
+
+# point loss configs
+# Number of points sampled during training for a mask point head.
+_C.MODEL.MASK_FORMER.TRAIN_NUM_POINTS = 112 * 112
+# Oversampling parameter for PointRend point sampling during training. Parameter `k` in the
+# original paper.
+_C.MODEL.MASK_FORMER.OVERSAMPLE_RATIO = 3.0
+# Importance sampling parameter for PointRend point sampling during training. Parametr `beta` in
+# the original paper.
+_C.MODEL.MASK_FORMER.IMPORTANCE_SAMPLE_RATIO = 0.75
+
