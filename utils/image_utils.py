c0bc7bf38dcbac3b49310b5e1f8f97377a02a5ed
Stefan Klut <stefanklut12@gmail.com>
2023-03-09 11:55:04 +0100

    documentation

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -5,7 +5,16 @@ from pathlib import Path
 import cv2
 import numpy as np
 
-def load_image_from_path(image_path: Path) -> Optional[np.ndarray]:
+def load_image_from_path(image_path: Path | str) -> Optional[np.ndarray]:
+    """
+    Load image from a given path, return None if loading failed due to corruption
+
+    Args:
+        image_path (Path | str): path to an image on current filesystem
+
+    Returns:
+        Optional[np.ndarray]: the loaded image or None
+    """
     #Supported: https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
     try:
         image = Image.open(image_path)
@@ -15,7 +24,16 @@ def load_image_from_path(image_path: Path) -> Optional[np.ndarray]:
         return None
     
 
-def load_image_from_bytes(img_bytes: bytes):
+def load_image_from_bytes(img_bytes: bytes) -> Optional[np.ndarray]:
+    """
+    Load image based on given bytes, return None if loading failed due to corruption
+
+    Args:
+        img_bytes (bytes): transfer bytes of data that represent an image
+
+    Returns:
+        Optional[np.ndarray]: the loaded image or None
+    """
     try:
         image = Image.open(BytesIO(img_bytes))
         image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
