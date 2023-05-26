1a6de6c5cc929290341943c43c0007aed7a819c6
Stefan Klut <stefanklut12@gmail.com>
2023-05-26 16:17:19 +0200

    speedtest, with lots of things commented out

--- speed_test.py
+++ speed_test.py
@@ -1,47 +1,73 @@
-import argparse
+# import argparse
 from multiprocessing import Pool
-import cv2
-from pathlib import Path
+import random
+# from multiprocessing.pool import ThreadPool as Pool
+# import cv2
+# from pathlib import Path
 
 from tqdm import tqdm
 
-from core.preprocess import preprocess_datasets
-from core.setup import setup_cfg
-from datasets.augmentations import build_augmentation
-from utils.tempdir import OptionalTemporaryDirectory
-from detectron2.data import DatasetMapper, build_detection_train_loader
+# import os
+# from typing import Optional, Sequence
+
+# from pathlib import Path
+
+# import functools
+# import inspect
+# import logging
+# from fvcore.common.config import CfgNode as _CfgNode
+
+from detectron2.utils.file_io import PathManager
+# from iopath.common.file_io import HTTPURLHandler, OneDrivePathHandler, PathHandler
+# from iopath.common.file_io import PathManager as PathManagerBase
+
+# from detectron2.config import CfgNode
+
+# from datasets import dataset
+# from datasets.preprocess import Preprocess
+# from utils.input_utils import clean_input_paths, get_file_paths
+# from page_xml.xml_converter import XMLConverter
+
+# from core.preprocess import preprocess_datasets
+# from core.setup import setup_cfg
+# from datasets.augmentations import build_augmentation
+# from utils.tempdir import OptionalTemporaryDirectory
+# from detectron2.data import DatasetMapper, build_detection_train_loader
 
 from utils.timing_utils import ContextTimer
 
-def get_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description="Main file for Layout Analysis")
+# def get_arguments() -> argparse.Namespace:
+#     parser = argparse.ArgumentParser(
+#         description="Main file for Layout Analysis")
 
-    detectron2_args = parser.add_argument_group("detectron2")
+#     detectron2_args = parser.add_argument_group("detectron2")
 
-    detectron2_args.add_argument(
-        "-c", "--config", help="config file", required=True)
-    detectron2_args.add_argument(
-        "--opts", nargs="+", help="optional args to change", default=[])
+#     detectron2_args.add_argument(
+#         "-c", "--config", help="config file", required=True)
+#     detectron2_args.add_argument(
+#         "--opts", nargs="+", help="optional args to change", default=[])
 
-    io_args = parser.add_argument_group("IO")
-    io_args.add_argument("-t", "--train", help="Train input folder/file",
-                            nargs="+", action="extend", type=str, required=True)
-    io_args.add_argument("-v", "--val", help="Validation input folder/file",
-                            nargs="+", action="extend", type=str, required=True)
+#     io_args = parser.add_argument_group("IO")
+#     io_args.add_argument("-t", "--train", help="Train input folder/file",
+#                             nargs="+", action="extend", type=str, required=True)
+#     io_args.add_argument("-v", "--val", help="Validation input folder/file",
+#                             nargs="+", action="extend", type=str, required=True)
     
-    tmp_args = parser.add_argument_group("tmp files")
-    tmp_args.add_argument(
-        "--tmp_dir", help="Temp files folder", type=str, default=None)
-    tmp_args.add_argument(
-        "--keep_tmp_dir", action="store_true", help="Don't remove tmp dir after execution")
+#     tmp_args = parser.add_argument_group("tmp files")
+#     tmp_args.add_argument(
+#         "--tmp_dir", help="Temp files folder", type=str, default=None)
+#     tmp_args.add_argument(
+#         "--keep_tmp_dir", action="store_true", help="Don't remove tmp dir after execution")
     
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    return args
+#     return args
 
-def load(x):
-    cv2.imread(str(x))
+# def load(x):
+#     cv2.imread(str(x))
+    
+def heavy(x):
+    return [random.randint(1, 100) * x for _ in range(1000000)]
 
 def main(args):
     
@@ -62,31 +88,35 @@ def main(args):
         
     #     dataloader = iter(build_detection_train_loader(cfg=cfg, mapper=mapper))
         
-    #     for i in range(100):
+    #     for i in tqdm(range(100)):
     #         print(i)
     #         with ContextTimer(label="Load"):
     #             next(dataloader)
-    paths = list(Path(args.train[0]).glob("*.jpg"))
+    # paths = list(Path(args.train[0]).glob("*.jpg"))
     with ContextTimer(), Pool(os.cpu_count()) as pool:
         # for image_path in tqdm(paths):
         #     cv2.imread(str(image_path))
-        _ = list(tqdm(pool.imap_unordered(load, paths), total=len(paths)))
+        # _ = list(tqdm(pool.imap_unordered(load, paths), total=len(paths)))
+        _ = list(tqdm(pool.imap_unordered(heavy, list(range(100))), total=100))
         
         
         
 
 if __name__ == "__main__":
-    args = get_arguments()
-    import torch
+    # args = get_arguments()
+    # import torch
     import os
     # os.sched_setaffinity(os.getpid(), list(range(20)))
     # os.system("taskset -p 0xFFFFFFFFFF %d" % os.getpid())
     import multiprocessing
     print(multiprocessing.cpu_count())
     
+    print(os.sched_getaffinity(0))
+    # os.sched_setaffinity(0, {3,4})
+    print(os.sched_getaffinity(0))
+    
     # torch.set_num_threads(100)
-    print(os.cpu_count(), torch.get_num_threads())
-    main(args)
+    main(args=None)
     print(ContextTimer.stats)
     import numpy as np
     print({key: np.mean(value) for key, value in ContextTimer.stats.items()})
