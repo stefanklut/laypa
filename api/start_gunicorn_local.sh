3a797475147b984094243290bae86150380ff017
Stefan Klut <stefanklut12@gmail.com>
2023-07-20 16:09:30 +0200

    local variables

--- api/start_gunicorn_local.sh
+++ api/start_gunicorn_local.sh
@@ -9,4 +9,10 @@ fi
 LAYPA_MAX_QUEUE_SIZE=16 \
 LAYPA_MODEL_BASE_PATH="/home/stefan/Documents/models/" \
 LAYPA_OUTPUT_BASE_PATH="/tmp/" \
+
+GUNICORN_RUN_HOST='0.0.0.0:5000'
+GUNICORN_WORKERS=1
+GUNICORN_THREADS=1
+GUNICORN_ACCESSLOG='-'
+
 python api/gunicorn_app.py
