8752a242c6155b8beda38dc0b59e2305bdc1df8c
Stefan Klut <stefanklut12@gmail.com>
2023-10-09 17:05:52 +0200

    move to get single print for warning

--- page_xml/xmlPAGE.py
+++ page_xml/xmlPAGE.py
@@ -423,16 +423,19 @@ class PageData:
         size = self.get_size()
         sem_seg = np.zeros(out_size, np.uint8)
         binary_mask = np.zeros(out_size, dtype=np.uint8)
+        overlap = False
         for baseline_coords in self._iter_baseline_coords():
             coords = self._scale_coords(baseline_coords, out_size, size)
             rounded_coords = np.round(coords).astype(np.int32)
             binary_mask.fill(0)
             cv2.polylines(binary_mask, [rounded_coords.reshape(-1, 1, 2)], False, baseline_color, line_width)
             
+            overlap = np.logical_or(overlap, np.any(np.logical_and(sem_seg, binary_mask)))
             # Add single line to full sem_seg
-            if np.any(np.logical_and(sem_seg, binary_mask)):
-                self.logger.warning(f"File {self.filepath} contains overlapping baselines")
             sem_seg = np.logical_or(sem_seg, binary_mask)
+        
+        if overlap:
+            self.logger.warning(f"File {self.filepath} contains overlapping baselines")
         if not sem_seg.any():
             self.logger.warning(f"File {self.filepath} does not contains baselines")
         return sem_seg
