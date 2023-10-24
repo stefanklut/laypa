8dd8362709b3a03c27f15866900234ab20e127fd
Stefan Klut <stefanklut12@gmail.com>
2023-10-24 13:36:06 +0200

    remove incorrect check

--- main.py
+++ main.py
@@ -122,8 +122,8 @@ def setup_training(args):
             trainer.resume_or_load(resume=cfg.MODEL.RESUME)
         else:
             trainer.checkpointer.load(cfg.TRAIN.WEIGHTS)
-            if trainer.checkpointer.has_checkpoint():
-                trainer.start_iter = trainer.iter + 1
+            # if trainer.checkpointer.has_checkpoint():
+            trainer.start_iter = trainer.iter + 1
         
         results = trainer.train()
 
